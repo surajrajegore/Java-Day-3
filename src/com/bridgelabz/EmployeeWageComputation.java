bae4591736a574d2aef1a0b09dad7af4831546ae
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-13 22:48:47 +0530

    Added:code to check max wage until reach max working hours

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -7,27 +7,29 @@ public class EmployeeWageComputation {
         final int FULL_TIME = 1;
         final int PART_TIME = 2;
         final int WAGE_PER_HOUR=20;
-        final int WORKING_DAYS = 20;
+        final int MAXI_WORKING_DAYS = 20;
+        final int MAXI_WORKING_HRS = 100;
         int totalWage = 0;
         int workingHours=0;
-        for (int day = 1; day <= WORKING_DAYS; day++)
+
+        for (int day = 1, totalWorkingHrs = 0; day <= MAXI_WORKING_DAYS
+                && totalWorkingHrs < MAXI_WORKING_HRS; day++, totalWorkingHrs += workingHours)
         {
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType) {
                 case FULL_TIME:
-                    System.out.println("Employee is Present Full time");
                     workingHours = 8;
                     break;
                 case PART_TIME:
-                    System.out.println("Employee is Present Part time");
                     workingHours = 4;
                     break;
                 default:
-                    System.out.println("Employee is Absent");
+                workingHours=0;
+                break;
             }
             int wage = workingHours * WAGE_PER_HOUR;
-            System.out.println("Day " + day + " wage is:" + wage);
             totalWage =totalWage + wage;
+
         }
         System.out.println("Total wage for a month is " + totalWage);
     }
